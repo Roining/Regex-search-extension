fc287d052d9f3bb1d8cc50dae8368b066a61905a
Flower <gataulin.ilia@gmail.com>
2020-11-14 01:35:49 +0400

    add scrollbar markers

--- src/js/content.js
+++ src/js/content.js
@@ -81,6 +81,21 @@ function highlight(regex, highlightColor, selectedColor, textColor, maxResults) 
         matchedTextNode.parentNode.replaceChild(spanNode, matchedTextNode);
         searchInfo.highlightedNodes.push(spanNode);
         searchInfo.length += 1;
+        const clientRect = spanNode.getBoundingClientRect();
+        if (clientRect.width && clientRect.height) {
+          const scrollMarker = document.createElement('div');
+          scrollMarker.className = 'highlighted_selection_scroll_marker';
+          scrollMarker.style.height = '2px';
+          scrollMarker.style.width = '16px';
+          scrollMarker.style.border = '1px solid grey';
+          scrollMarker.style.boxSizing = 'content-box';
+          scrollMarker.style.backgroundColor = 'yellow';
+          scrollMarker.style.position = 'fixed';
+          scrollMarker.style.top = `${window.innerHeight * (clientRect.top + (clientRect.top - clientRect.bottom) / 2 + window.scrollY) / document.body.clientHeight}px`;
+          scrollMarker.style.right = '0px';
+          scrollMarker.style.zIndex = '2147483647';
+          document.body.appendChild(scrollMarker);
+        }
         return 1;
       }
     } else if (isExpandable(node)) {
