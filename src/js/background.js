5e9a301c42f64cc43d4d84dfecddc8bd0f104093
Flower <gataulin.ilia@gmail.com>
2020-11-15 00:39:18 +0400

    add selection autocomplete

--- src/js/background.js
+++ src/js/background.js
@@ -1,10 +1,16 @@
 /* Received returnSearchInfo message, set badge text with number of results */
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if ('returnSearchInfo' == request.message) {
-    console.log("twiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiihhhhhhhhh");
+    
     chrome.browserAction.setBadgeText({
       'text': String(request.numResults),
       'tabId': sender.tab.id
     });
   }
+});
+document.addEventListener('selectionchange', () => {
+  console.log("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+  var test = window.getSelection().toString();
+  document.getElementById('inputRegex').value = test;
+
 });
