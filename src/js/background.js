d670b6c8c01472b7d27bf95e925180c69efaa89c
Flower <gataulin.ilia@gmail.com>
2020-11-15 00:16:15 +0400

    add selection autocomplete

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,7 @@
 /* Received returnSearchInfo message, set badge text with number of results */
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if ('returnSearchInfo' == request.message) {
+    console.log("twiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiihhhhhhhhh");
     chrome.browserAction.setBadgeText({
       'text': String(request.numResults),
       'tabId': sender.tab.id
