6f1faecf7231b73e6bf009c8940bac67a3a10c41
Flower <gataulin.ilia@gmail.com>
2021-05-29 16:32:57 +0400

    final version

--- src/js/content.js
+++ src/js/content.js
@@ -152,21 +152,26 @@ function rainbow(numOfSteps, step) {
   // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
   // Adam Cole, 2011-Sept-14
   // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
-  var r, g, b;
-  var h = step / numOfSteps;
-  var i = ~~(h * 6);
-  var f = h * 6 - i;
-  var q = 1 - f;
-  switch(i % 6){
-      case 0: r = 1; g = f; b = 0; break;
-      case 1: r = q; g = 1; b = 0; break;
-      case 2: r = 0; g = 1; b = f; break;
-      case 3: r = 0; g = q; b = 1; break;
-      case 4: r = f; g = 0; b = 1; break;
-      case 5: r = 1; g = 0; b = q; break;
+  var color = '';
+  switch(step){
+      case 0: color = '#FFD700'; break;
+      case 1: color = '#ADFF2F'; break;
+      case 2: color = '#87CEEB'; break;
+      case 3: color = '#FF0000'; break;
+      case 4: color = '#EE82EE'; break;
+      case 5: color = '#FFA500'; break;
+      case 6: color = '#008000'; break;
+      case 7: color = '#4169E1'; break;
+      case 8: color = '#00FFFF'; break;
+      case 9: color = '#F0E68C'; break;
+      case 10: color = '#BC8F8F'; break;
+      case 11: color = '#BC8F8F'; break;
+      case 12: color = '#BC8F8F'; break;
+      case 13: color = '#BC8F8F'; break;
+      case 14: color = '#BC8F8F'; break;
   }
-  var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
-  return (c);
+  //var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+  return (color);
 }
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
